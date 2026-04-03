c40777110e62a5dec1094caefa055cc16c5e929b
Kid Max <kidmax285@gmail.com>
2026-04-03 20:40:15 +0600

    refactor: make process user data payload readonly

--- app/Events/ProcessUserData.php
+++ app/Events/ProcessUserData.php
@@ -9,5 +9,5 @@ class ProcessUserData
     /**
      * Create a new event instance.
      */
-    public function __construct(private ?array $appData = []) {}
+    public function __construct(private readonly ?array $appData = []) {}
 }
